9eb8f967bbd9043520677185b083086aafb87ca5
DarinaKir <dasha23kir@gmail.com>
2022-11-15 16:24:58 +0200

    15.11.2022

--- WordleJS.js
+++ WordleJS.js
@@ -1,13 +1,44 @@
-function addALetter(letter){
-    let isAdd =false;
+let currentSquareId = 0;
+let words = ["תהילה", "דרינה", "אבטיח", "ישראל", "חופשה", "בדידה", "הייטק", "פנינה", "פרגית", "מרוקו", "גלידה", "מושלם"];
 
-    for (let i = 1; i < 7 && !isAdd; i++) {
-        for (let j = 1; j < 6  && !isAdd; j++) {
-            let element1 = document.getElementById("cell"+i+''+j);
-            if (element1.innerText === ''){
-                element1.innerText = letter;
-                isAdd = true;
-            }
+function isCellPainted (id){
+    let cellElement = document.getElementById(id);
+    return (cellElement.classList[cellElement.classList.length-1] != "square")
+}
+
+function addALetter(letter){
+    if ((currentSquareId == 0) || (currentSquareId % 5 != 0) || isCellPainted(currentSquareId)){
+        currentSquareId ++;
+        document.getElementById(currentSquareId+"").innerText = letter;
+        // document.getElementById(currentSquareId+"").classList.add("wrong");
+    }
+}
+
+function validInput (){
+    if ((currentSquareId != 0) && (currentSquareId % 5 == 0) && !isCellPainted(currentSquareId)){
+        for (let i = 0; i < 5; i++) {
+            let currentSquare = document.getElementById((currentSquareId-i) + "");
+            if (words[0].charAt(4-i) == currentSquare.innerText){
+                currentSquare.classList.add("right");
+            }else if (words[0].includes(currentSquare.innerText)){
+                currentSquare.classList.add("wrongPlace");
+            }else currentSquare.classList.add("wrong");
         }
+    }
+}
+
+// function isLetterExist (letter){
+//     for (let i = 0; i < words[0].length; i++) {
+//         if (words[0].charAt(i) === letter){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+function deleteLetter (){
+    if ((currentSquareId != 0) && !isCellPainted(currentSquareId)){
+        document.getElementById(currentSquareId+"").innerText = "";
+        currentSquareId --;
     }
 }

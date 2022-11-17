22229d5250b18e219caada0f645af85dc79d34fc
DarinaKir <dasha23kir@gmail.com>
2022-11-17 17:24:43 +0200

    17.11.2022

--- WordleJS.js
+++ WordleJS.js
@@ -1,9 +1,9 @@
+const WORDS = ["תהילה", "דרינה", "אבטיח", "ישראל", "חופשה", "בדידה", "הייטק", "פנינה", "פרגית", "מרוקו", "גלידה", "מושלם"];
 let currentSquareId = 0;
-let words = ["תהילה", "דרינה", "אבטיח", "ישראל", "חופשה", "בדידה", "הייטק", "פנינה", "פרגית", "מרוקו", "גלידה", "מושלם"];
 
 function isCellPainted (id){
     let cellElement = document.getElementById(id);
-    return (cellElement.classList[cellElement.classList.length-1] != "square")
+    return (cellElement.classList[cellElement.classList.length-1] != null)
 }
 
 function addALetter(letter){
@@ -14,13 +14,20 @@ function addALetter(letter){
     }
 }
 
+function deleteLetter (){
+    if ((currentSquareId != 0) && !isCellPainted(currentSquareId)){
+        document.getElementById(currentSquareId+"").innerText = "";
+        currentSquareId --;
+    }
+}
+
 function validInput (){
     if ((currentSquareId != 0) && (currentSquareId % 5 == 0) && !isCellPainted(currentSquareId)){
         for (let i = 0; i < 5; i++) {
             let currentSquare = document.getElementById((currentSquareId-i) + "");
-            if (words[0].charAt(4-i) == currentSquare.innerText){
+            if (WORDS[0].charAt(4-i) == currentSquare.innerText){
                 currentSquare.classList.add("right");
-            }else if (words[0].includes(currentSquare.innerText)){
+            }else if (WORDS[0].includes(currentSquare.innerText)){
                 currentSquare.classList.add("wrongPlace");
             }else currentSquare.classList.add("wrong");
         }
@@ -28,17 +35,28 @@ function validInput (){
 }
 
 // function isLetterExist (letter){
-//     for (let i = 0; i < words[0].length; i++) {
-//         if (words[0].charAt(i) === letter){
+//     for (let i = 0; i < WORDS[0].length; i++) {
+//         if (WORDS[0].charAt(i) === letter){
 //             return true;
 //         }
 //     }
 //     return false;
 // }
 
-function deleteLetter (){
-    if ((currentSquareId != 0) && !isCellPainted(currentSquareId)){
-        document.getElementById(currentSquareId+"").innerText = "";
-        currentSquareId --;
+function determiningId (){
+    let cells = document.getElementsByTagName("td");
+    let counter = 1;
+    for (let i = 0; i < 6; i++) {
+        for (let j = 5; j > 0; j--) {
+            cells[(j+(i*5))-1].id = counter;
+            counter++;
+        }
+    }
+    let buttons = document.getElementsByTagName("button");
+    for (let i = 0; i < buttons.length; i++) {
+        if (buttons[i].innerText.length == 1){
+            buttons[i].id = buttons[i].innerText;
+            buttons[i].classList.add("letter");
+        }
     }
 }
